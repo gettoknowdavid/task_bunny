91e83cdcb7af16ddd452b216ddfa220f3ef89967
David Michael II <knowdavidmichael@gmail.com>
2024-06-14 03:17:38 +0100

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_bunny
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/task_bunny/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_bunny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
