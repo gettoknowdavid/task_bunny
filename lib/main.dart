121a1bb13d2ebf937e1d42f9912c0d929ca25640
David Michael II <knowdavidmichael@gmail.com>
2024-06-15 12:29:15 +0100

    config: initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app_tb.dart';
 import 'core/core.dart';
+import 'firebase_options.dart';
 
 FutureOr<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   // Initializes all dependencies before running the application
   configureDependencies();
-  
+
   runApp(const AppTB());
 }
