8f2f52017309c199e75955d0a5f80b13c839959b
David Michael II <knowdavidmichael@gmail.com>
2024-06-16 17:08:21 +0100

    add screen util package for adaptive screen size and texts

--- lib/app_tb.dart
+++ lib/app_tb.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/core.dart';
+import 'features/auth/auth.dart';
 
 class AppTB extends StatelessWidget {
   const AppTB({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: TBNavigation.instance.router,
-      theme: TBTheme.light,
-      darkTheme: TBTheme.dark,
-      themeMode: ThemeMode.light,
+    final navigation = TBNavigation(authBloc: context.watch<AuthBloc>());
+    final textScaler = TextScaler.linear(1.sp);
+
+    return ScreenUtilInit(
+      ensureScreenSize: true,
+      builder: (context, child) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: navigation.router,
+        theme: TBTheme.light,
+        darkTheme: TBTheme.dark,
+        themeMode: ThemeMode.light,
+        builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaler: textScaler),
+          child: child!,
+        ),
+      ),
     );
   }
 }
